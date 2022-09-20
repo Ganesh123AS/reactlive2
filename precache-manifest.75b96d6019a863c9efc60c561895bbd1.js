97e6042effbcc82b34e7ab7542c1b6e75c699551
Ganesh123AS <ganeshbhatt818@gmail.com>
2022-09-20 12:41:02 +0545

    Updates

---
+++ precache-manifest.75b96d6019a863c9efc60c561895bbd1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1b168e4bb23736c3a76",
+    "url": "/reactlive2/static/css/main.7f1600be.chunk.css"
+  },
+  {
+    "revision": "f1b168e4bb23736c3a76",
+    "url": "/reactlive2/static/js/main.f1b168e4.chunk.js"
+  },
+  {
+    "revision": "14b060c7915162ae1f5f",
+    "url": "/reactlive2/static/js/1.14b060c7.chunk.js"
+  },
+  {
+    "revision": "b68f95687c670bc6aa40",
+    "url": "/reactlive2/static/css/2.f78bbc34.chunk.css"
+  },
+  {
+    "revision": "b68f95687c670bc6aa40",
+    "url": "/reactlive2/static/js/2.b68f9568.chunk.js"
+  },
+  {
+    "revision": "148313f992bf1fec2f6d",
+    "url": "/reactlive2/static/js/runtime~main.148313f9.js"
+  },
+  {
+    "revision": "27e08f7ca7806da33b00ad19cf0d4475",
+    "url": "/reactlive2/static/media/dslr3.27e08f7c.jpg"
+  },
+  {
+    "revision": "ae2d23ee3394a2289e234b6e98e1c0a6",
+    "url": "/reactlive2/static/media/dslr4.ae2d23ee.jpg"
+  },
+  {
+    "revision": "a755b139132460ec4d3e5cca2283cb53",
+    "url": "/reactlive2/static/media/sandal1.a755b139.jpg"
+  },
+  {
+    "revision": "db769f5a131b957c38fd123483634256",
+    "url": "/reactlive2/static/media/sandal2.db769f5a.jpg"
+  },
+  {
+    "revision": "6c3b8272e7b90d6f4e556983238b22bb",
+    "url": "/reactlive2/static/media/mobile5.6c3b8272.jpg"
+  },
+  {
+    "revision": "dd7e6caff3aeedede79060288231a0a8",
+    "url": "/reactlive2/static/media/hoodie1.dd7e6caf.jpg"
+  },
+  {
+    "revision": "6f38edfbf2e7645cd6349f8167fae1f9",
+    "url": "/reactlive2/static/media/gym-bg.6f38edfb.jpg"
+  },
+  {
+    "revision": "7e9170985f8dfad07c7c1a15d7dc8321",
+    "url": "/reactlive2/index.html"
+  }
+];
